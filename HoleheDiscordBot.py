00dd1804cdb768793363845bb146f92aa88290f2
J/ <55988027+jkerai1@users.noreply.github.com>
2024-03-17 17:10:16 +0000

    Create HoleheDiscordBot.py

---
+++ HoleheDiscordBot.py
@@ -0,0 +1,40 @@
+import discord as discord
+from discord.ext import commands
+from discord import Message
+import asyncio
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv("holehe.env")
+token = os.getenv("Token")
+intents = discord.Intents.default()
+intents.message_content = True 
+client = commands.Bot(command_prefix = '?', intents = intents)
+
+
+async def blocking_function(email_address):  
+    print('entering blocking function')
+    output = subprocess.check_output(['holehe','--only-used',str(email_address)])
+    output = output.decode('utf8').replace("'", '"')
+    output = output[153:-234].replace("32m[+","HIT FOUND").replace("[32","").replace("[0m","") 
+    return output
+
+@client.command(aliases = ["holehe","osint"])
+async def OSINT(ctx,message):
+    """ Runs holehe against Email in message"""
+    if len(message) > 4: #Lazy Error Handle
+        email_address = message.replace(" ","").replace(",",".").replace("#","")
+        print(email_address)
+        output =  await blocking_function(email_address)
+        print(output)
+    else: 
+        output = "Invalid Input"
+    await ctx.send(output) #Test ?holehe test@gmail.com
+                   
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    
+
+await client.start(token)#client.run(token) if running from non-notebook
