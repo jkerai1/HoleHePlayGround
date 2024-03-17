e557d751cefbef40632f4b9d0e42c909099d21e0
J/ <55988027+jkerai1@users.noreply.github.com>
2024-03-17 12:31:00 +0000

    Update TeamsWebHook.py

--- TeamsWebHook.py
+++ TeamsWebHook.py
@@ -1,10 +1,10 @@
 import subprocess
 import pymsteams
-myTeamsMessage = pymsteams.connectorcard("<Microsoft Webhook URL>") ##replace with Teams URL
-email_address = "test@gmail.com" ##Grab From Webhook pushed by adaptive card
+myTeamsMessage = pymsteams.connectorcard("<Microsoft Webhook URL>") #replace with Teams Webhook URL
+email_address = "test@gmail.com" #Push in your test Email.
 output = subprocess.check_output(['holehe','--only-used',str(email_address.replace(" ","").replace(",","."))])
 my_json = output.decode('utf8').replace("'", '"')
-my_json = my_json = my_json[150:-234]#.replace("\n","<br>")
+my_json = my_json = my_json[150:-234] #.replace("\n","<br>"), if working in HTML
 print(my_json)
 myTeamsMessage.text(my_json)
 myTeamsMessage.send()
